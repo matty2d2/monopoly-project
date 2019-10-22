14816179b4fc308c7e7db2df1cac75a3c2376d8a
github username <github email address>
2019-10-22 09:32:38 +0100

    setup game logic (with if statements)

--- monopoly-backend/app/controllers/players_controller.rb
+++ monopoly-backend/app/controllers/players_controller.rb
@@ -14,8 +14,6 @@ class PlayersController < ApplicationController
         player = Player.find(params[:id])
         player.cash = params[:cash]
         player.currently_on = params[:currently_on]
-
-        byebug
         player.save
         render json: PlayerSerializer.new(player).to_serialized_json
     end
